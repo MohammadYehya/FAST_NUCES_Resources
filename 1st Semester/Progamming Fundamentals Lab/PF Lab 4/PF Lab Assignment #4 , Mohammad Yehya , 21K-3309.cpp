89adad855b679eab11160be2073289c4640ecb96
MohammadYehya <mohammad7446@gmail.com>
2025-04-28 21:18:54 +0500

    PF Lab 4

---
+++ 1st Semester/Progamming Fundamentals Lab/PF Lab 4/PF Lab Assignment #4 , Mohammad Yehya , 21K-3309.cpp
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	//Q1
+//	int StdID,Courses;
+//	int Fee = 15000;
+//	printf("INPUT the student ID: ");
+//	scanf("%d",&StdID);
+//	printf("Input the total number of courses: ");
+//	scanf("%d",&Courses);
+//	printf("Cost of each course: %d", Fee);
+//	printf("\n\nStudent ID = %d",StdID);
+//	printf("\nTotal Amount = %d",Fee * Courses);
+	//Q2
+//	float Num1,Num2;
+//	float Quo;
+//	printf("Please enter two numbers\nNumber1 = ");
+//	scanf("%f", &Num1);
+//	printf("Number2 = ");
+//	scanf("%f",&Num2);
+//	Quo = Num1/Num2;
+//	printf("The sum of the two numbers is = %f", Num1 + Num2);
+//	printf("\nThe product of the two numbers is = %f", Num1*Num2);
+//	printf("\nThe difference of the two numbers is = %f",Num1 - Num2);
+//	printf("\nThe quotient of the two numbers is = %f", Quo);
+//	printf("\nThe remainder of the two numbers is = %f",fmod(Num1,Num2));
+	//Q3
+//	int x;
+//	x = 7 + 3 * 6 / 2 - 1;
+//	printf("First 6 is divided by 2 then the answer is multiplied by 3 and then added by 7 and -1\nThe number is %d",x) ;
+//	x = 2 % 2 + 2 * 2 - 2 / 2;
+//	printf("\nFirst 2 MOD 2 is done then 2*2 then 2/2 then all of them are added\nThe number is %d",x);
+//	x = ( 3 * 9 * ( 3 + ( 9 * 3 / ( 3 ) ) ) );
+//	printf("\nFirst the inner most bracket is solved and then the next bracket and so on\nThe number is %d",x);
+	//Q4
+//	float Num;
+//	printf("Please enter a number: ");
+//	scanf("%f", &Num);
+//	printf("The numbers are %.1f , %.1f , %.1f", Num , pow(Num,2) , pow(Num,3));
+	//Q5
+//	float S,a,t,u;
+//	printf("Please enter the initial velocity of the object: ");
+//	scanf("%f", &u);
+//	printf("Please enter the acceleration of the object: ");
+//	scanf("%f", &a);
+//	printf("Please enter the time taken of the object: ");
+//	scanf("%f", &t);
+//	S= u*t + 0.5*a*pow(t,2);
+//	printf("The displacement of the object is %f", S);
+	//Q6
+//	float r;
+//	const float pi = 3.142;
+//	printf("Please enter radius of the sphere: ");
+//	scanf("%f", &r);
+//	printf("The volume of the sphere is = %f", (1.333333) * (pi * pow(r,3)));
+//	float P;
+//	float R;
+//	float t;
+//	float CP;
+//	printf("\n\nPlease enter principle amount: ");
+//	scanf("%f",&P);
+//	printf("Please enter interest rate: ");
+//	scanf("%f", &R);
+//	printf("Please enter time: ");
+//	scanf ("%f",&t);
+//	CP = P * pow(1+(R/100),t);
+//	printf("The compound interest over %.1f years is = %f", t,CP);
+	//Q7
+//	int UK = 10;
+//	int US = 6;
+//	int CHN = 4;
+//	int AUS = 4;
+//	printf("Julia had %d UK coins , %d US coins , %d Chinese coins , %d Australian coins.", UK,US,CHN,AUS);
+//	printf("\nNow Julia has %d UK coins , %d US coins , %d Chinese coins , %d Australian coins.", UK-4,US-2,CHN-1);
+	//Q8
+//	float weight_p,weight_kg,height_I,height_m;
+//	printf("Please enter weight in pounds: ");
+//	scanf("%f", &weight_p);
+//	weight_kg = weight_p / 2.2046;
+//	printf("Please enter height in inches: ");
+//	scanf("%f", &height_I);
+//	height_m = height_I * 0.0254;
+//	printf("The weight in Kilograms is = %f", weight_kg);
+//	printf("\nThe height in metres is = %f", height_m);
+	
+}
