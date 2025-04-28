6fd7b6efce80fbf78496063812ac759697df5796
MohammadYehya <mohammad7446@gmail.com>
2025-04-28 21:19:13 +0500

    PF Lab 5

---
+++ 1st Semester/Progamming Fundamentals Lab/PF Lab 5/PF Lab Assignment #5 , Mohammad Yehya , 21K-3309.cpp
@@ -0,0 +1,177 @@
+#include <stdio.h>
+int main(){
+	//Q1
+//	int num;
+//	printf("Please enter a number: ");
+//	scanf("%d",&num);
+//	if (num%3 == 0 && num%6 == 0)
+//		printf("This number is a multiple of 6 and 3");
+//	else 
+//		(printf("This number is not a multiple of 6 and 3 "));
+	//Q2
+//	int hrs;
+//	printf("Please enter time of the day in 24 hour system: ");
+//	scanf("%d",&hrs);
+//	switch(hrs){
+//		case 5 ... 11:
+//			printf("Good Morning");
+//			break;
+//		case 12 ... 18 :
+//			printf("Good Evening");
+//			break;
+//		case 19 ... 24 :
+//			printf("Good Night");
+//			break;
+//		default:
+//			printf("Invalid Input");
+//			break;
+//	}
+	//Q3
+//	float a1,a2,a3;
+//	printf("Please enter the angles of a triagle");
+//	printf("\nAngle 1 = ");
+//	scanf("%f", &a1);
+//	printf("Angle 2 = ");
+//	scanf("%f", &a2);
+//	printf("Angle 3 = ");
+//	scanf("%f", &a3);
+//	if ((a1 + a2 + a3) == 180)
+//		printf("It is a valid triangle");
+//	else 
+//		printf("Not a valid triangle"); 
+	//Q4
+//	float op1,op2;
+//	char oper;
+//	printf("Please enter the operator(+ - * /): ");
+//	scanf("%c",&oper);
+//	printf("Please enter the operands");
+//	printf("\nOperand1 = ");
+//	scanf("%f",&op1);
+//	printf("Operand2 = ");
+//	scanf("%f",&op2);
+//	switch(oper){
+//		case('+'):
+//			printf("%f + %f = %f",op1,op2,op1+op2);
+//			break;
+//		case('-'):
+//			printf("%f - %f = %f",op1,op2,op1-op2);
+//			break;
+//		case('*'):
+//			printf("%f * %f = %f",op1,op2,op1*op2);
+//			break;
+//		case('/'):
+//			if (op2 != 0){
+//				printf("%f / %f = %f",op1,op2,op1/op2);}
+//			else printf("The second operator can not be zero.");
+//			break;
+//		default:
+//			printf("Invalid Operator");
+//			break;
+//	}
+	//Q5
+//	float unit;
+//	printf("Please enter units of electricity used: ");
+//	scanf("%f",&unit);
+//	if(unit>0 && unit<=50){
+//		printf("Bill for this month is = %f", unit * 0.75);}
+//	else if(unit>50 && unit<=150){
+//			printf("Bill for this month is = %f", (unit - 50)*1.5 + 50 * 0.75);}
+//		else if(unit>150 && unit<=250){
+//			printf("Bills for this month is = %f",(unit - 150)* 1.75 + 100 * 1.5 + 50 * 0.75);}
+//			else if(unit>250){
+//				printf("Bills for this month is = %f", unit * 2);}
+//					else printf("Invalid Input");
+	//Q6
+//	float num1,num2,num3;
+//	printf("Please enter three numbers");
+//	printf("\nNumber 1 = ");
+//	scanf("%f",&num1);
+//	printf("Number 2 = ");
+//	scanf("%f",&num2);
+//	printf("Number 3 = ");
+//	scanf("%f",&num3);
+//	if (num1 == num2 && num2 == num3)
+//		{
+//			printf("All numbers are equal");
+//		}
+//	else if (num1 != num2 && num2 != num3 && num1 != num3)
+//		{
+//			printf("All numbers are different");
+//		}
+//	else printf("Neither all are equal nor different");
+	//Q7
+//	int num;
+//	printf("Please enter number: ");
+//	scanf("%d",&num);
+//	switch(num){
+//		case 1:
+//			printf("one");
+//			break;
+//		case 2:
+//			printf("two");
+//			break;
+//		case 3:
+//			printf("three");
+//			break;
+//		case 4:
+//			printf("four");
+//			break;
+//		case 5:
+//			printf("five");
+//			break;
+//		case 6:
+//			printf("six");
+//			break;
+//		case 7:
+//			printf("seven");
+//			break;
+//		case 8:
+//			printf("eight");
+//			break;
+//		case 9:
+//			printf("nine");
+//			break;
+//		case 10:
+//			printf("ten");
+//			break;
+//		default:
+//			printf("Out of range");
+//	}
+	//Q8
+//	float dist;
+//	printf("Please enter distance travelled = ");
+//	scanf("%f",&dist);
+//	if (dist>0 && dist<=100)
+//		{
+//			printf("Cost = 5");
+//		}
+//	else if (dist>100 && dist<=500)
+//		{
+//			printf("Cost = 8");
+//		}
+//	else if (dist>500 && dist<=1000)
+//		{
+//			printf("Cost = 10");
+//		}
+//	else if (dist>1000)
+//		{
+//			printf("Cost = 12");
+//		}
+//	else printf("Invalid Input");
+	//Q9
+//	int num;
+//	printf("Please enter a number: ");
+//	scanf("%d", &num);
+//	if (num%2 != 0)
+//		{
+//			num = num*3 + 1;
+//			printf("The new value of the number is = %d", num);
+//		}
+//	else 
+//		{
+//			num = num / 2;
+//			printf("The new value of the number is = %d", num);
+//		}
+}
+	
+	
