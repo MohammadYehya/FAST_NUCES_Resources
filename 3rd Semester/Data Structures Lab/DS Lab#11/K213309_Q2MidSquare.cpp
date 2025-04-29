9e72b8425a83035ddc7e93f67c693e8ba3dde3d8
MohammadYehya <mohammad7446@gmail.com>
2025-04-30 00:09:48 +0500

    DS Lab

---
+++ 3rd Semester/Data Structures Lab/DS Lab#11/K213309_Q2MidSquare.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+template<typename T, int n>
+class HashMapTable
+{
+	private:
+		class HashTableEntry	//Encapsulated Class is unaccessible in main
+		{
+			public:
+				T val;
+				int key;
+				HashTableEntry() {}
+				HashTableEntry(T val, int key) {this->val = val; this->key = key;}
+		} **table;
+	public:
+		HashMapTable() 
+		{
+			table = new HashTableEntry*[n];
+			for(int i = 0 ; i < n ; i++) table[i] = NULL;
+		}
+		~HashMapTable()
+		{
+			for(int i = 0 ; i < n ; i++) if(table[i] != NULL) delete table[i];
+			delete [] table;
+		}
+		int NumberOfDigits(int v)
+		{
+			int c = 0;
+			while(v > 0)
+			{
+				c++;
+				v /= 10;
+			}
+			return c;
+		}
+		int HashFunc(int v)
+		{
+			v *= v;
+			int d = pow(10,NumberOfDigits(v)-2);
+			if(!d)d = n;
+			return v / 10 % d;
+		}
+		void Insert(T val, int k)
+		{
+			int _k = HashFunc(val);
+			if(table[_k] != NULL) delete table[_k];
+			table[_k] = new HashTableEntry(val, k);
+		}
+		T Search(int k)
+		{
+			if(table[k] != NULL) return table[k]->val;
+		}
+		void Remove(int k)
+		{
+			if(table[k] != NULL) 
+			{
+				delete table[k];
+				table[k] = NULL;
+			}
+		}
+		void PrintMap()
+		{
+			for(int i = 0 ; i < n ; i++)
+			{
+				cout << i << ")";
+				if(table[i] != NULL)cout << table[i]->val << "|" << table[i]->key << endl;
+				else cout << "0|0" << endl;
+			}
+		}
+};
+int main()
+{
+	HashMapTable<int,10> h;
+	h.Insert(11,6);
+	h.Insert(2,16);
+	h.PrintMap();
+	cout << endl << h.Search(2) << endl << endl;
+	h.Remove(0);
+	h.PrintMap();
+}
+
